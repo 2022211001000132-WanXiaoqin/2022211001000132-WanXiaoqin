d5533b99de51d83bb66359b196a266b482ff666c
2022211001000132-WanXiaoqin <2578182860@qq.com>
2024-03-25 21:55:21 +0800

    HW-Week 4 Commit

--- src/main/java/com/example/WanXiaoqin2022211001000132/HelloServlet.java
+++ src/main/java/com/example/WanXiaoqin2022211001000132/HelloServlet.java
@@ -1,10 +1,20 @@
 package com.example.WanXiaoqin2022211001000132;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello-servlet",
+initParams={
+        @WebInitParam(name="driver",value="com.mysql.cj.jdbc.Driver"),
+@WebInitParam(name="url",value="jdbc:mysql://localhost:3306/userdb"),
+@WebInitParam(name="username",value="root"),
+@WebInitParam(name="password",value="123456"),
+        }
+        )
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -20,6 +30,19 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        String driver="com.mysql.cj.jdbc.Driver";
+        String url="jdbc:mysql://localhost:3306/userdb";
+        String username="root";
+        String password="123456";
+
+
+        try{
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url,username,password);
+            System.out.println(con);
+        } catch (ClassNotFoundException | SQLException  e){
+            e.printStackTrace() ;
+        }
     }
 
     public void destroy() {
